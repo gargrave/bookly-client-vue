d4d1cb9502f5c1b88559e6db4aca6f2f2625ed4d
Gabe Hargrave <gkhargrave@gmail.com>
2017-06-09 13:21:57 -0700

    Set up basic account/profile detail page

---
+++ src/components/auth/components/AccountDetailView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card-content">
+    <p><strong>Username:</strong> {{ userData.username }} </p>
+    <p><strong>Real name:</strong> {{ profile.firstName }} {{ profile.lastName }} </p>
+    <p><strong>Email:</strong> {{ userData.email }} </p>
+    <p><strong>Member since:</strong> {{ userData.created }} </p>
+
+    <hr>
+    <button
+      class="primary"
+      @click="$emit('editClicked')">
+      Edit
+    </button>
+
+    <button
+      class="secondary pull-right"
+      @click="$emit('logoutClicked')">
+        Logout
+    </button>
+  </div><!-- /card-content -->
+</template>
+
+<script>
+export default {
+  props: {
+    userData: { type: Object, required: true },
+    profile: { type: Object, required: true }
+  }
+}
+</script>
--- src/components/auth/containers/AccountDetailPage.vue
+++ src/components/auth/containers/AccountDetailPage.vue
@@ -1,18 +1,106 @@
 <template>
-  <div class="layout-view">
-    <div class="row justify-center">
+  <transition name="fade">
+    <div class="layout-view">
+      <div class="row justify-center">
 
-      <div class="card">
+        <div class="card">
 
-        <div class="card-title bg-primary text-white">
-          Account Detail Page
-        </div><!-- /card-title -->
+          <div class="card-title bg-primary text-white">
+            My Profile
+          </div><!-- /card-title -->
 
-        <div class="card-content">
-        </div><!-- /card-content -->
+          <app-account-detail-view
+            :userData="userData"
+            :profile="{}"
+            @editClicked="onEditClick"
+            @logoutClicked="onLogoutClick">
+          </app-account-detail-view>
 
-      </div><!-- /card -->
+        </div><!-- /card -->
 
-    </div><!-- /row -->
-  </div><!-- /layout-view -->
+      </div><!-- /row -->
+    </div><!-- /layout-view -->
+  </transition>
 </template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+import { Loading, Toast } from 'quasar'
+
+import { localUrls } from '../../../globals/urls'
+
+import AccountDetailView from '../components/AccountDetailView'
+
+export default {
+  components: {
+    appAccountDetailView: AccountDetailView
+  },
+
+  data () {
+    return {
+      initializing: true,
+      // whether any operations are currently running
+      working: false,
+      // error messages returned from API (e.g. invalid data)
+      apiError: '',
+      // whether we are in editing or viewing mode
+      editing: false
+    }
+  },
+
+  computed: {
+    ...mapGetters([
+      'userData',
+      'profile'
+    ])
+  },
+
+  methods: {
+    /**
+     * Callback for clicking the 'edit' button; simply change to 'editing' state.
+     */
+    onEditClick () {
+      this.editing = true
+    },
+
+    /**
+     * Callback for clicking the 'logout' button; simply logout.
+     */
+    onLogoutClick () {
+      this.working = true
+      Loading.show({ message: 'Logging out...' })
+
+      this.logout()
+        .then(() => {
+          Toast.create.info('Logged out!')
+          this.$router.push(localUrls.login)
+          this.working = false
+          Loading.hide()
+        })
+    },
+
+    ...mapActions([
+      'checkForStoredLogin',
+      'logout'
+    ])
+  },
+
+  created () {
+    // redirect to login page if not logged in
+    this.working = true
+    Loading.show({ message: 'Loading...' })
+
+    this.checkForStoredLogin()
+      .then(() => {
+        this.working = false
+        this.initializing = false
+        Loading.hide()
+      }, () => {
+        this.$router.push(localUrls.login)
+        this.working = false
+        this.initializing = false
+        Loading.hide()
+      })
+  }
+}
+</script>
