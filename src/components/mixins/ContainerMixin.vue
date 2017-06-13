ee9fb6d5063d546bc7f2eab9074c86a8d52cb5ba
Gabe Hargrave <gkhargrave@gmail.com>
2017-06-13 14:57:28 -0700

    Create generic ContainerMixin for use by all container components

---
+++ src/components/mixins/ContainerMixin.vue
@@ -0,0 +1,42 @@
+<script>
+import { Loading } from 'quasar'
+
+export default {
+  data: () => ({
+    // whether the component is going through first initialization
+    initializing: true,
+    // whether any operations are currently running
+    working: false,
+    // error messages returned from API (e.g. invalid data)
+    apiError: ''
+  }),
+
+  methods: {
+    /**
+     * Enters working state by setting the necessary vars.
+     */
+    enterWorkingState () {
+      this.working = true
+      this.initializing = true
+      Loading.show({ message: 'Working...' })
+    },
+
+    /**
+     * Exits 'working' state by resetting all associated vars.
+     */
+    exitWorkingState () {
+      this.working = false
+      this.initializing = false
+      Loading.hide()
+    },
+
+    /** Gracefully handles any error messages from the API */
+    onError (err) {
+      this.apiError = err.message || ''
+      this.working = false
+      this.initializing = false
+      Loading.hide()
+    }
+  }
+}
+</script>
