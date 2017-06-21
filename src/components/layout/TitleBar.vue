6dacf68c8f5663cd0f20f7e433ea2dc3b1e6d3df
Gabe Hargrave <gkhargrave@gmail.com>
2017-06-21 11:25:23 -0700

    Moderately large TitleBar overhaul (see notes)
    - Use the updated styles to alter the sizing and padding of Quasar's default toolbar
    - Manually layout "q-toolbar-content" and "q-toolbar-title" elements, rather than using the directives--the way it did it by default wouldn't let me insert the links the way I wanted, but this simple fix gets around that.
    - Add links to "Books" and "Authors" list page in toolbar on large sizes

--- src/components/layout/TitleBar.vue
+++ src/components/layout/TitleBar.vue
@@ -1,19 +1,54 @@
 <template>
   <div class="toolbar">
+
     <button
       class="hide-on-drawer-visible"
-      @click="$emit('toggleDrawer')"
-    >
+      @click="$emit('toggleDrawer')">
       <i>menu</i>
     </button>
 
-    <q-toolbar-title :padding="0" class="toolbar-title">
-      Bookly
-    </q-toolbar-title>
-  </div>
+    <div class="toolbar-content">
+
+      <span class="toolbar-element toolbar-title">Bookly</span>
+
+      <!-- toolbar links; only shown on lg sizes -->
+      <span class="toolbar-links gt-sm inline">
+        <router-link
+          class="toolbar-element toolbar-link"
+          active-class="toolbar-link-active"
+          :to="{ name: routes.books.list }">
+          Books
+        </router-link>
+
+        <router-link
+          class="toolbar-element toolbar-link"
+          active-class="toolbar-link-active"
+          :to="{ name: routes.authors.list }">
+          Authors
+        </router-link>
+      </span>
+
+    </div><!-- /toolbar-content -->
+
+  </div><!-- /toolbar -->
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
+import { routes } from '../../globals/urls'
+
 export default {
+  data () {
+    return {
+      routes // expose to template
+    }
+  },
+
+  computed: {
+    ...mapGetters([
+      'isLoggedIn'
+    ])
+  }
 }
 </script>
