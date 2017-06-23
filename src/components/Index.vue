aeed831b5def8c32e1a5abddaa90addfa206a76d
Gabe Hargrave <gkhargrave@gmail.com>
2017-06-23 08:59:02 -0700

    Add 'fade transition' to index page and update the speed of it

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,78 +1,80 @@
 <template>
-  <div class="layout-view">
-    <div class="row justify-center">
+  <transition name="fade">
+    <div class="layout-view">
+      <div class="row justify-center">
 
-      <div class="card index-page-card">
+        <div class="card index-page-card">
 
-        <div class="card-title bg-primary text-white">
-          Welcome to Bookly!
-        </div><!-- /card-title -->
+          <div class="card-title bg-primary text-white">
+            Welcome to Bookly!
+          </div><!-- /card-title -->
 
-        <div class="card-content" v-if="isLoggedIn">
-          <p>
-            The <em>only</em> place on the Internet where you can track your favorite
-            authors <em>and</em> their books!
-          </p>
+          <div class="card-content" v-if="isLoggedIn">
+            <p>
+              The <em>only</em> place on the Internet where you can track your favorite
+              authors <em>and</em> their books!
+            </p>
 
-          <hr>
-          <p>You are currently logged in as <strong>{{ userData.email }}</strong>.</p>
+            <hr>
+            <p>You are currently logged in as <strong>{{ userData.email }}</strong>.</p>
 
-          <hr>
-          <p>Looking for something to do?</p>
-          <br>
+            <hr>
+            <p>Looking for something to do?</p>
+            <br>
 
-          <p>
-            Create a new
-            <router-link :to="{ name: routes.authors.create }">Author</router-link>
-            or
-            <router-link :to="{ name: routes.books.create }">Book</router-link>.
-          </p>
+            <p>
+              Create a new
+              <router-link :to="{ name: routes.authors.create }">Author</router-link>
+              or
+              <router-link :to="{ name: routes.books.create }">Book</router-link>.
+            </p>
 
-          <p>
-            View your existing
-            <router-link :to="{ name: routes.authors.list }">Authors</router-link>
-            or
-            <router-link :to="{ name: routes.books.list }">Books</router-link>.
-          </p>
+            <p>
+              View your existing
+              <router-link :to="{ name: routes.authors.list }">Authors</router-link>
+              or
+              <router-link :to="{ name: routes.books.list }">Books</router-link>.
+            </p>
 
-          <p>
-            View your
-            <router-link :to="{ name: routes.auth.detail }">Account</router-link>.
-          </p>
+            <p>
+              View your
+              <router-link :to="{ name: routes.auth.detail }">Account</router-link>.
+            </p>
 
-        </div><!-- /card-content -->
+          </div><!-- /card-content -->
 
-        <div class="card-content" v-else>
-          <p>
-            The <em>only</em> place on the Internet where you can track your favorite authors <em>and</em> their books!
-          </p>
-          <p>
-            While other apps will only let you track authors, we have cracked the technology required to
-            keep track of books as well, so prepare yourself for a truly next-gen experience!
-          </p>
+          <div class="card-content" v-else>
+            <p>
+              The <em>only</em> place on the Internet where you can track your favorite authors <em>and</em> their books!
+            </p>
+            <p>
+              While other apps will only let you track authors, we have cracked the technology required to
+              keep track of books as well, so prepare yourself for a truly next-gen experience!
+            </p>
 
-          <hr>
+            <hr>
 
-          <p>Log in or sign up to get started!</p>
+            <p>Log in or sign up to get started!</p>
 
-          <p>
-            <button
-              class="primary"
-              @click="$router.push(localUrls.account)">
-              Log In
-            </button>
-            <button
-              class="secondary"
-              @click="$router.push(localUrls.register)">
-              Sign Up
-            </button>
-          </p>
-        </div><!-- /card-content -->
+            <p>
+              <button
+                class="primary"
+                @click="$router.push(localUrls.account)">
+                Log In
+              </button>
+              <button
+                class="secondary"
+                @click="$router.push(localUrls.register)">
+                Sign Up
+              </button>
+            </p>
+          </div><!-- /card-content -->
 
-      </div><!-- /card -->
+        </div><!-- /card -->
 
-    </div><!-- /row -->
-  </div><!-- /layout-view -->
+      </div><!-- /row -->
+    </div><!-- /layout-view -->
+  </transition>
 </template>
 
 <script>
