e76e0bd8ce9005c3017c0554d700989706dbc7fb
Gabe Hargrave <gkhargrave@gmail.com>
2017-06-29 11:56:32 -0700

    Refactor: rebuild to export 'routes' by name--this was needed for the tests

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import { localUrls, routes } from './globals/urls'
+import { localUrls, routes as _routes } from './globals/urls'
 
 import AccountDetailPage from './components/auth/containers/AccountDetailPage'
 import LoginPage from './components/auth/containers/LoginPage'
@@ -24,97 +24,85 @@ function load (component) {
   return () => System.import(`components/${component}.vue`)
 }
 
-export default new VueRouter({
-  /*
-   * NOTE! VueRouter "history" mode DOESN'T works for Cordova builds,
-   * it is only to be used only for websites.
-   *
-   * If you decide to go with "history" mode, please also open /config/index.js
-   * and set "build.publicPath" to something other than an empty string.
-   * Example: '/' instead of current ''
-   *
-   * If switching back to default "hash" mode, don't forget to set the
-   * build publicPath back to '' so Cordova builds work again.
-   */
+export const routes = [
+  /* ============================================
+   = Auth routes
+   ============================================= */
+  {
+    path: localUrls.login,
+    component: LoginPage,
+    name: _routes.auth.login
+  },
+  {
+    path: localUrls.account,
+    component: AccountDetailPage,
+    name: _routes.auth.detail
+  },
+  {
+    path: localUrls.register,
+    component: RegisterPage,
+    name: _routes.auth.register
+  },
+  {
+    path: localUrls.verify,
+    component: VerifyAccountPage,
+    name: _routes.auth.verify
+  },
 
-  routes: [
-    /* ============================================
-     = Auth routes
-     ============================================= */
-    {
-      path: localUrls.login,
-      component: LoginPage,
-      name: routes.auth.login
-    },
-    {
-      path: localUrls.account,
-      component: AccountDetailPage,
-      name: routes.auth.detail
-    },
-    {
-      path: localUrls.register,
-      component: RegisterPage,
-      name: routes.auth.register
-    },
-    {
-      path: localUrls.verify,
-      component: VerifyAccountPage,
-      name: routes.auth.verify
-    },
+  /* ============================================
+   = Password reset routes
+   ============================================= */
+  {
+    path: localUrls.pwResetRequest,
+    component: PasswordResetRequestPage,
+    name: _routes.auth.pwReset
+  },
+  {
+    path: localUrls.pwResetConfirm,
+    component: PasswordResetPage,
+    name: _routes.auth.pwResetConfirm
+  },
 
-    /* ============================================
-     = Password reset routes
-     ============================================= */
-    {
-      path: localUrls.pwResetRequest,
-      component: PasswordResetRequestPage,
-      name: routes.auth.pwReset
-    },
-    {
-      path: localUrls.pwResetConfirm,
-      component: PasswordResetPage,
-      name: routes.auth.pwResetConfirm
-    },
+  /* ============================================
+   = Authors routes
+   ============================================= */
+  {
+    path: localUrls.authorCreate,
+    component: AuthorCreatePage,
+    name: _routes.authors.create
+  },
+  {
+    path: localUrls.authorDetail,
+    component: AuthorDetailPage,
+    name: _routes.authors.detail
+  },
+  {
+    path: localUrls.authorsList,
+    component: AuthorsListPage,
+    name: _routes.authors.list
+  },
 
-    /* ============================================
-     = Authors routes
-     ============================================= */
-    {
-      path: localUrls.authorCreate,
-      component: AuthorCreatePage,
-      name: routes.authors.create
-    },
-    {
-      path: localUrls.authorDetail,
-      component: AuthorDetailPage,
-      name: routes.authors.detail
-    },
-    {
-      path: localUrls.authorsList,
-      component: AuthorsListPage,
-      name: routes.authors.list
-    },
+  /* ============================================
+   = Books routes
+   ============================================= */
+  {
+    path: localUrls.bookCreate,
+    component: BookCreatePage,
+    name: _routes.books.create
+  },
+  {
+    path: localUrls.bookDetail,
+    component: BookDetailPage,
+    name: _routes.books.detail
+  },
+  {
+    path: localUrls.booksList,
+    component: BooksListPage,
+    name: _routes.books.list
+  },
 
-    /* ============================================
-     = Books routes
-     ============================================= */
-    {
-      path: localUrls.bookCreate,
-      component: BookCreatePage,
-      name: routes.books.create
-    },
-    {
-      path: localUrls.bookDetail,
-      component: BookDetailPage,
-      name: routes.books.detail
-    },
-    {
-      path: localUrls.booksList,
-      component: BooksListPage,
-      name: routes.books.list
-    },
+  { path: '/', component: load('Index') }, // Default
+  { path: '*', component: load('Error404') } // Not found
+]
 
-    { path: '/', component: load('Index') }, // Default
-    { path: '*', component: load('Error404') } // Not found
-  ]
-})
+export default new VueRouter({ routes })
